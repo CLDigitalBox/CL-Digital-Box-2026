8b1471a3b9242fe091ddfca98b1eaa325232c21d
CLDigitalBox <processosgerais22@gmail.com>
2026-01-17 16:03:41 -0300

    Refactor vite.config.js by cleaning up comments
    Removed commented-out configuration and adjusted plugin imports.

--- vite.config.js
+++ vite.config.js
@@ -6,24 +6,17 @@ import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
 import { creaoPlugins } from "./config/vite/creao-plugin.mjs";
+...
+plugins: [
+  // ...creaoPlugins(),
+  TanStackRouterVite({
+    autoCodeSplitting: false,
+  }),
+  viteReact({ jsxRuntime: "automatic" }),
+  svgr(),
+  tailwindcss(),
+],
 
-// https://vitejs.dev/config/
-export default defineConfig({
-	base: process.env.TENANT_ID ? `/${process.env.TENANT_ID}/` : "/",
-	define: {
-		"import.meta.env.TENANT_ID": JSON.stringify(process.env.TENANT_ID || ""),
-	},
-	plugins: [
-		...creaoPlugins(),
-		TanStackRouterVite({
-			autoCodeSplitting: false, // affects pick-n-edit feature. disabled for now.
-		}),
-		viteReact({
-			jsxRuntime: "automatic",
-		}),
-		svgr(),
-		tailwindcss(),
-	],
 	test: {
 		globals: true,
 		environment: "jsdom",
